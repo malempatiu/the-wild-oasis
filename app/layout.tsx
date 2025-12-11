67efd7c42f341d4ae19452119f232307cbbe25ef
ugesh malempati <malempatiu@gmail.com>
2025-12-11 20:03:20 +0100

    add basic Navigation component for the whole app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import { Logo } from "./_components/Logo";
+import { Navigation } from "./_components/Navigation";
+
+export const metadata = {
+  title: "The Wild Oasis",
+  description: "A sanctuary for nature lovers and adventurers.",
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +13,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
-    <h1>The wild oasis</h1>
+    <div>
+      <h1>The wild oasis</h1>
+    </div>
   );
-}
+};
 
 export default Page;
