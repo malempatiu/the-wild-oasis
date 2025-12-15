f34cd0c9b0f3d4057b328555f1b693384410a016
ugesh malempati <malempatiu@gmail.com>
2025-12-15 12:22:54 +0100

    build home, cabins, and about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import "./_styles/global.css";
-import { Logo } from "./_components/Logo";
-import { Navigation } from "./_components/Navigation";
+import "@/app/_styles/global.css";
 import {Josefin_Sans} from "next/font/google";
-import { Header } from "./_components/Header";
+import { Header } from "@/app/_components/Header";
 
 const josefinSans = Josefin_Sans({
   subsets: ['latin'],
@@ -25,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={josefinSans.className}>
-      <body className='antialiased bg-primary-950 text-primary-50 min-h-screen flex flex-col'>
+      <body className='antialiased bg-primary-950 text-primary-50 min-h-screen flex flex-col relative'>
         <Header />
         <div className='flex-1 px-8 py-12'>
           <main className='max-w-7xl mx-auto w-full'>{children}</main>
